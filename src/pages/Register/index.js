a9c4be9dd4d83c23edb338b85e7e2aa9ea3f9de7
khoirxz <khoirrurrohman@gmail.com>
2020-12-04 16:11:10 +0700

    styling register page

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,9 +1,26 @@
 import React from "react";
+import "./register.scss";
+import { RegisterBg } from "../../assets";
+import { Input, Button, Gap, Link } from "../../components";
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
+    <div className="main-page">
+      <div className="left">
+        <img src={RegisterBg} className="bg-image" alt="Register" />
+      </div>
+      <div className="right">
+        <p className="title">Register</p>
+        <Input label="Full name" placeholder="Full name" />
+        <Gap height={18} />
+        <Input label="Email" placeholder="Email" />
+        <Gap height={18} />
+        <Input label="Password" placeholder="Password" />
+        <Gap height={50} />
+        <Button title="Daftar" />
+        <Gap height={100} />
+        <Link title="kembali ke Login" />
+      </div>
     </div>
   );
 };
